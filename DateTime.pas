f673dd13642cce251fefb3e1c35802e8203ab0ce
megavvve <sakopov@sfedu.ru>
2022-10-06 08:57:07 +0300

    Проверка секунд

--- DateTime.pas
+++ DateTime.pas
@@ -1,3 +1,10 @@
 ﻿begin
+  var d := datetime.Now.Year;
+  if (d mod 4 = 0) and ((d mod 100 <> 0) or (d mod 400 = 0)) then
+    println(d,'- вискокосный')
+  else
+    println(d,'- невискокосный');
   
+  var p := 1;
+  print(p*60,$'- Секунд в {p} минуте')
 end.
